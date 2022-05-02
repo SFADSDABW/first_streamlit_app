8ad92818df7be898a6f97a7d2e09e7e19c24169b
Tony <104785638+SFADSDABW@users.noreply.github.com>
2022-05-02 13:41:25 -0400

    Update streamlit_app.py
    Fixing errors because I didn't know the syntax!!

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('My First streamlit file in Python')
 
 streamlit.header('Breakfast of Champion Coders')
-Coffee
-More Coffee
-Crumpets
-Even More Coffee
+streamlit.text('Coffee')
+streamlit.text('More Coffee')
+streamlit.text('Crumpets')
+streamlit.text('Even More Coffee')
 
