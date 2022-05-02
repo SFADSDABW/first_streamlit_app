3f6cbe9b0436931e5ca305d9543ca07446132430
Tony <104785638+SFADSDABW@users.noreply.github.com>
2022-05-02 14:02:26 -0400

    Update streamlit_app.py
    oops, should have been a plural!!

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add in a multi-select widget
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Apple','Strawberry'])
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Apple','Strawberries'])
 # Display it
 streamlit.dataframe(my_fruit_list)
