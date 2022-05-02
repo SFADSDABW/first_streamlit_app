f5e44b86a473f22819c74ab95a029fc31c502153
Tony <104785638+SFADSDABW@users.noreply.github.com>
2022-05-02 13:59:36 -0400

    Update streamlit_app.py
    Hmm, added an index item so that the name is dplaiyed in the selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('Even More Coffee')
 
 # start importing data into Python and displaying it
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add in a multi-select widget
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
