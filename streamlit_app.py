34fa4543045f7a3fb8e231db8e1dc3f0bc2775c2
Tony <104785638+SFADSDABW@users.noreply.github.com>
2022-05-05 06:09:20 -0400

    Update streamlit_app.py
    changed variable to be add_my_fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")         
 streamlit.dataframe(my_data_rows)
 
-fruit_to_add = streamlit.text_input('What Fruit would you like to add?','Kiwi')
-streamlit.write('The User entered',fruit_to_add)
+add_my_fruit = streamlit.text_input('What Fruit would you like to add?','Kiwi')
+streamlit.write('The User entered',add_my_fruit)
 
 # my_cur.execute("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
 # streamlit.text("Hello from Snowflake:")
