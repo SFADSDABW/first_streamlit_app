8d07b7da18d68fb89a44820a677102796bb0f2e7
Tony <104785638+SFADSDABW@users.noreply.github.com>
2022-05-05 07:38:53 -0400

    Update streamlit_app.py
    Added fundtion to retrieve the user choice for fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+
 streamlit.title('My First streamlit file in Python')
 
 streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
@@ -24,6 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display it
 streamlit.dataframe(fruits_to_show)
 
+# Create the repeatable code block (function)
+def get_fruity_advice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 # New Section to display fruityvace api response
 streamlit.header('Fruityvice Fruit Advice')
 try:
@@ -31,9 +38,8 @@ try:
   if not fruit_choice:
     streamlit.write('Please select a fruit to get information')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruity_advice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
     
 except URLError as e:
     streamlit.error()
